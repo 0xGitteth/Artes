970bde35271b41e1f6fa2ee81fc21b7a041c3d2f
0xGitteth <160280586+0xGitteth@users.noreply.github.com>
2026-03-05 20:46:00 +0100

    Use brand icon assets for like and comment buttons

--- src/components/icons/LikeIcon.jsx
+++ src/components/icons/LikeIcon.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import MaskIcon from './MaskIcon';
 
 export default function LikeIcon(props) {
-  return <MaskIcon src="/icons/applause.svg" activeColor="#D4A017" {...props} />;
+  return <MaskIcon src="/icons/Applause.svg" activeColor="#D4A017" {...props} />;
 }

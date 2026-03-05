c17791afbbf739fca38563734afea6a28318548d
0xGitteth <160280586+0xGitteth@users.noreply.github.com>
2026-03-05 18:08:30 +0100

    Adjust like/comment icon colors and remove action borders

---
+++ src/components/icons/CommentIcon.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MaskIcon from './MaskIcon';
+
+export default function CommentIcon(props) {
+  return <MaskIcon src="/icons/comment.svg" activeColor="#2563eb" {...props} />;
+}
